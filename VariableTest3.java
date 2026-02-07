66facfb409e2e9f19612e17266b55a374ee85911
U <Miya15255702294@outlook.com>
2026-02-07 20:41:31 +0800

    also day02
    I study too slowly~ And i can't stop to entertain by smartphone. Oh no, that's so bad......

---
+++ VariableTest3.java
@@ -0,0 +1,18 @@
+public class VariableTest3{
+	public static void main(String[]args){
+		//定义变量记录电影的名字
+		String name="人狼游戏";
+		//定义变量记录主演的名字
+		String tostar="樱庭奈奈美 竹富圣花";
+		//定义变量记录电影的年份
+		int year=2013;
+		//定义变量记录电影的评分
+		double grade=6.3;
+		
+		//输出所有变量的值
+		System.out.println(name);
+		System.out.println(tostar);
+		System.out.println(year);
+		System.out.println(grade);
+	}
+}
