47d68c848ebccc581e8600fe5f48fe94a3e8a4a0
But-Omit <Miya15255702294@outlook.com>
2026-02-21 18:28:05 +0800

    also day04

---
+++ basic-code/day04-code/src/com/itheima/test/Test15.java
@@ -0,0 +1,11 @@
+package com.itheima.test;
+
+public class Test15 {
+    public static void main(String[] args) {
+        int sum=0;
+        for(int i=1;i<=100;i++){//100.fori
+            if(i%2==0) sum+=i;
+        }
+        System.out.println(sum);
+    }
+}
